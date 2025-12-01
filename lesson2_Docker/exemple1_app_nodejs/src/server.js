181e77102de430e660327ed80bd3111baa5cfc76
zyko <zyko.taouaf@gmail.com>
2025-12-01 16:55:03 +0100

    Small updates

--- lesson2_Docker/exemple1_app_nodejs/src/server.js
+++ lesson2_Docker/exemple1_app_nodejs/src/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <h1>Welcome to my awesome app!</h1>
+    <h1>Zakaria TAOUAF :)))</h1>
     <img src="https://i.pinimg.com/originals/cb/22/60/cb22603604e36d1caab7cc4e729506c9.gif" alt="Beautiful Landscape" style="max-width:400px;">
   `);
 });
